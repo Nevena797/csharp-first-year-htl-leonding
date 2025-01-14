7b5f696189cf1303410ad8ece9f6e965be3dde37
Nevena <nevena.rogova@gmx.at>
2025-01-14 12:26:26 +0200

    Add files via upload

---
+++ YOH.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("+-----------------------------+");
+Console.WriteLine("|                             |");
+Console.WriteLine("| ##    ##  ###   ##  ##  ##  |");
+Console.WriteLine("|  ##  ##  ## ##  ##  ##  ##  |");
+Console.WriteLine("|   ####  ##   ## ######  ##  |");
+Console.WriteLine("|    ##   ##   ## ######  ##  |");
+Console.WriteLine("|    ##    ## ##  ##  ##      |");
+Console.WriteLine("|    ##     ###   ##  ##  ##  |");
+Console.WriteLine("|                             |");
+Console.WriteLine("+-----------------------------+");
+
+
+
+Console.WriteLine("+-----------------------------+");
+Console.WriteLine("|                             |");
+Console.WriteLine("| ##    ##  ###   ##  ##  ##  |");
+Console.Write("| ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write(" ##  ##  ## ##  ##  ##  ##  ");
+Console.ResetColor();
+Console.WriteLine("|");
+Console.Write("| ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("  ####  ##   ## ######  ##  ");
+Console.ResetColor();
+Console.WriteLine("|");
+Console.ResetColor();
+Console.WriteLine("|    ##   ##   ## ######  ##  |");
+Console.WriteLine("|    ##    ## ##  ##  ##      |");
+Console.WriteLine("|    ##     ###   ##  ##  ##  |");
+Console.WriteLine("|                             |");
+Console.WriteLine("+-----------------------------+");
+
+
