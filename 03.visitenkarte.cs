95dcf7fed5aba04b19e58e1d4520dafc5db83ccd
Nevena <nevena.rogova@gmx.at>
2025-01-18 17:27:34 +0200

    Add files via upload

---
+++ 03.visitenkarte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+Console.WriteLine("Visitenkarte");
+Console.WriteLine("=====================================");
+Console.WriteLine();
+
+Console.Write("Bitte geben sie ihren Vornamen ein: ");
+var vorname = Console.ReadLine();
+
+Console.Write("Bitte geben sie ihren Nachnamen ein: ");
+var nachname = Console.ReadLine();
+
+
+Console.WriteLine("*******************");
+Console.WriteLine($"* {vorname,-15} ");
+Console.WriteLine("*******************");
+Console.WriteLine($"* {nachname,-15} ");
+Console.WriteLine("*******************");
+
+Console.WriteLine();
+
+Console.WriteLine("*******************");
+Console.WriteLine($"* {vorname,-15} *");
+Console.WriteLine("*******************");
+Console.WriteLine($"* {nachname,-15} *");
+Console.WriteLine("*******************");
