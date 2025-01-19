1fd64b1a69f068d846555ca3a5d76881127ffa5b
Nevena <nevena.rogova@gmx.at>
2025-01-19 18:22:08 +0200

    Add files via upload

---
+++ 13.DayOfYearWithoutLeapSwitch.cs
@@ -0,0 +1,10 @@
+﻿namespace _13.DayOfYearWithoutLeapSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
