23f58ffae66063e6e2cc516990886f97acc0da98
Nevena <nevena.rogova@gmx.at>
2025-01-19 12:23:00 +0200

    Add files via upload

---
+++ 07.Uhrzeit1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.WriteLine("Uhrzeitumrechnung in 00:00:00");
+Console.WriteLine("=============================");
+Console.WriteLine();
+
+Console.Write("Please enter amount of seconds [0.. 86459]:");
+string input = Console.ReadLine();
+int totalSeconds = int.Parse(input);
+
+
+
+int seconds = totalSeconds % 60;
+int totalMinutes = totalSeconds / 60;
+int minutes = totalMinutes % 60;
+int hours = totalMinutes / 60;
+
+Console.WriteLine($"{hours:00}:{minutes:00}:{seconds:00}");
