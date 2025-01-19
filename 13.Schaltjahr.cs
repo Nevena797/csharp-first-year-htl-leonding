d70fc7115cf6d70671c058dd29a38f97b2567047
Nevena <nevena.rogova@gmx.at>
2025-01-19 18:41:57 +0200

    Add files via upload

---
+++ 13.Schaltjahr.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+string input;
+int year;
+
+Console.Write("Bitte geben sie eine Jahreszahl ein: ");
+input = Console.ReadLine();
+year = int.Parse(input);
+
+if (year % 4 == 0)
+{
+    if (year % 100 == 0)
+    {
+        if (year % 400 == 0)
+        {
+            Console.WriteLine("Schaltjahr");
+        }
+        else
+        {
+            Console.WriteLine("Kein Schaltjahr");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Schaltjahr");
+    }
+}
+else
+{
+    Console.WriteLine("Kein Schaltjahr");
+}   
