2e50cace699ebc0bf1ad4967376cecebd4ec8838
Nevena <nevena.rogova@gmx.at>
2025-01-21 13:55:56 +0200

    Add files via upload

---
+++ 24.tryParseDemo.cs
@@ -0,0 +1,19 @@
+﻿bool isOK;
+int myValue;
+
+do
+{
+    Console.Write("Please enter value [0..100]: ");
+    isOK =
+        int.TryParse(Console.ReadLine(), out myValue) &&
+        myValue >= 0 &&
+        myValue <= 100;
+
+    if (!isOK)
+    {
+        Console.WriteLine("ungültiger Wert!");
+    }
+
+} while (!isOK);
+
+Console.WriteLine($"Value={myValue}");
