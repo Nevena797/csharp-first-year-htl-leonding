4f5faabe3bad1faed3a7fde1e7cf4bdeeb0f3430
Nevena <nevena.rogova@gmx.at>
2025-01-21 09:21:26 +0200

    Add files via upload

---
+++ 05.greeting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+Console.WriteLine($"Bitte geben Sie Ihren Vornamen ein: ");
+string firstName = Console.ReadLine();
+Console.WriteLine($"Bitte geben Sie Ihren Nachnamen ein: ");
+string lastName = Console.ReadLine();
+Console.WriteLine($"Bitte geben Sie Ihren Wohnort ein: ");
+string town = Console.ReadLine();
+Console.WriteLine($"Herzlich Willkommen {firstName} {lastName} aus {town} zu C#!");
+
+
+
+
+//        string firstName;
+//        string lastName;
+//        string city;
+//
+//        Console.Write("Bitte geben Sie Ihren Vornamen ein: ");
+//        firstName = Console.ReadLine();
+
+//        Console.Write("Bitte geben Sie Ihren Nachnamen ein: ");
+//        lastName = Console.ReadLine();
+
+//        Console.Write("Bitte geben Sie Ihren Wohnort ein: ");
+//        city = Console.ReadLine();
+
+//      
+//        Console.Write("Herzlich Willkommen ");
+//        Console.Write(firstName);
+//        Console.Write(" ");
+//        Console.Write(lastName);
+//        Console.Write(" aus ");
+//        Console.Write(city);
+//        Console.WriteLine(" zu C#!");
+//    }
+//} 
