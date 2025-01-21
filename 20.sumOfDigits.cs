294ef9537cde2627736811542b3b74bcf6faa0af
Nevena <nevena.rogova@gmx.at>
2025-01-21 11:15:21 +0200

    Add files via upload

---
+++ 20.sumOfDigits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+Console.Write("Bitte geben Sie einen Wert ein: ");
+int val = int.Parse(Console.ReadLine());
+int sumOfDigits = 0;
+
+while (val > 0)
+{
+    int digit = val % 10;
+
+    val /= 10;      //val = val /10;
+
+    sumOfDigits += digit;
+
+}
+Console.WriteLine($"Digit = {sumOfDigits}");
+
