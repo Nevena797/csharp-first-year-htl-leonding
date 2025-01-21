4920441d34be61afb289de445d9ba21e5481c223
Nevena <nevena.rogova@gmx.at>
2025-01-21 16:27:34 +0200

    Add files via upload

---
+++ 25.einMaleins.cs
@@ -0,0 +1,47 @@
+﻿int size;
+bool isOK;
+do
+{
+    Console.WriteLine("Please enter column/row count [0..31]: ");
+    isOK = int.TryParse(Console.ReadLine(), out size) &&
+           size >= 0 &&
+           size <= 31;
+
+    if (!isOK)
+    {
+        Console.WriteLine("Ungültiger Wert!");
+    }
+} while (!isOK); 
+
+
+if (size == 0)
+{
+    Console.WriteLine("+");
+}
+else
+{
+
+    for (int row = 1; row <= size; row++) 
+    {
+        Console.Write("+");
+        for (int col = 1; col <= size; col++) 
+        {
+            Console.Write("---+");
+        }
+        Console.WriteLine();
+
+        for (int col = 1; col <= size; col++) 
+        {
+            Console.Write($"|{row * col,3}"); 
+        }
+        Console.WriteLine("|");
+    }
+
+
+    Console.Write("+");
+    for (int col = 1; col <= size; col++)
+    {
+        Console.Write("---+");
+    }
+    Console.WriteLine();
+}
