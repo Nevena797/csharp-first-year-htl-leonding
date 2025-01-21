7eaf197fbc2bbc5fdfaf1be40b262888a3e60d40
Nevena <nevena.rogova@gmx.at>
2025-01-21 10:27:02 +0200

    Add files via upload

---
+++ 19.decimalToBinary2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+Console.WriteLine("Convert decimal to binary");
+Console.WriteLine("======================================");
+Console.WriteLine();
+Console.WriteLine("Please enter decimal value [int,>=0]: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+
+var remainingNumber = decimalNumber;
+string binaryString = "";
+while (remainingNumber > 0)
+{
+    var remaining = remainingNumber % 2;
+    remainingNumber = remaining / 2;
+
+    binaryString = remaining + binaryString;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Decimal number: {decimalNumber} as binary: {binaryString}");
