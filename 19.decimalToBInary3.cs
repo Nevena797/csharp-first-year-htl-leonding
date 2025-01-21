23cdd4e3364c46f61c7a63989125a46df6b5ff0c
Nevena <nevena.rogova@gmx.at>
2025-01-21 10:25:51 +0200

    Add files via upload

---
+++ 19.decimalToBInary3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+Console.Write("Please enter a decimal value [0..] (-1 for exit): ");
+int decimalValue = int.Parse(Console.ReadLine());
+
+while (decimalValue != -1)
+{
+    int remaining = decimalValue;
+    string binaryString = "";
+
+    if (decimalValue == 0)
+    {
+        binaryString = "0";
+    }
+    else
+    {
+        if (remaining < 0)
+        {
+            remaining = -remaining;
+        }
+
+        while (remaining != 0)
+        {
+            int rem = remaining % 2;
+            remaining /= 2;
+
+            binaryString = rem + binaryString;
+        }
+    }
+
+    if (decimalValue < 0)
+    {
+        binaryString = "-" + binaryString;
+    }
+
+    Console.WriteLine($"Decimal: {decimalValue} => {binaryString}");
+
+    Console.Write("Please enter a decimal value [0..] (-1 for exit): ");
+    decimalValue = Convert.ToInt32(Console.ReadLine());
+}
