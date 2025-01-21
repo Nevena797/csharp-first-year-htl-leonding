3c29226317fec41ec7d7812a27b4d0d7e1415521
Nevena <nevena.rogova@gmx.at>
2025-01-21 14:33:32 +0200

    Add files via upload

---
+++ 23.factoriceNumber.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Please enter a positive integer (> 1): ");
+int number = int.Parse(Console.ReadLine());
+int originalNumber = number; 
+
+Console.Write($"{originalNumber} = 1");
+
+int divisor = 2; 
+while (number > 1)
+{
+    while (number % divisor == 0) 
+    {
+        Console.Write($" * {divisor}");
+        number /= divisor; 
+    }
+    divisor++; 
+}
+
+Console.WriteLine();
+
