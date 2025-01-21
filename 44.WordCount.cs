d9dfc6030e9985f18d76a312d2b1ec012390b5b7
Nevena <nevena.rogova@gmx.at>
2025-01-21 14:44:59 +0200

    Add files via upload

---
+++ 44.WordCount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+static int CountWords(string sentence)
+{
+    if (string.IsNullOrWhiteSpace(sentence))  // Prüft ob der String leer oder nur Leerzeichen enthält
+        return 0;
+
+    int wordCount = 0;
+    bool inWord = false;
+
+    for (int i = 0; i < sentence.Length; i++)
+    {
+        if (char.IsWhiteSpace(sentence[i]))
+        {
+            inWord = false;
+        }
+        else if (!inWord)
+        {
+            inWord = true;
+            wordCount++;
+        }
+    }
+
+    return wordCount;
+}
+
+Console.WriteLine("Count words in a sentence");
+Console.WriteLine("****************************");
+
+while (true)
+{
+    Console.Write("Sentence: ");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "end")
+        break;
+
+    int words = CountWords(input);
+    Console.WriteLine($"The sentence \"{input}\" has {words} word{(words != 1 ? "s" : "")}.");
+}
