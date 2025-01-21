177adbc46b5d5ca7b15dc5ab2ce75549a7916c2d
Nevena <nevena.rogova@gmx.at>
2025-01-21 10:08:47 +0200

    Add files via upload

---
+++ 18.decimalTobinary.cs
@@ -0,0 +1,18 @@
+﻿string output = " ";
+
+Console.Write("Bitte geben Sie einen Wert ein: ");
+int val = int.Parse(Console.ReadLine());
+
+while (val > 0)
+{
+    int digit = val % 2;
+
+    val /= 2;
+
+    output = $"{digit} {output}";
+
+
+}
+Console.WriteLine(output);
+
+
