a0a9674182ecdb201e398565151ccb4c59bc00f9
Nevena <nevena.rogova@gmx.at>
2025-02-04 15:18:49 +0200

    Add files via upload

---
+++ leerspaces.cs
@@ -0,0 +1,12 @@
+﻿
+int SkipLeadingSpaces(string str, int startIdx)
+{
+    for (int i = startIdx; i < str.Length; i++)
+    {
+        if (!char.IsWhiteSpace(str[i]))
+        {
+            return i;
+        }
+    }
+    return str.Length;
+}
