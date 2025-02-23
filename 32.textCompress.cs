f249dd7a02eb7ab2fde7d18f4669c169e7e97ce8
Nevena <nevena.rogova@gmx.at>
2025-02-23 21:16:54 +0200

    Add files via upload

---
+++ 32.textCompress.cs
@@ -0,0 +1,45 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 3 ACIF
+ *--------------------------------------------------------------
+ *              Nevena Rogova
+ *--------------------------------------------------------------
+ * Description:
+ * BinSearch
+ *--------------------------------------------------------------
+*/
+using System;
+
+Console.Write("Please enter a sentence: ");
+
+string input = Console.ReadLine();
+string output = "";
+
+int startIdx = 0;
+
+while (startIdx < input.Length && input[startIdx] == ' ')
+{
+    startIdx++;
+}
+
+int idx = startIdx;
+
+while (idx < input.Length)
+{
+    if (!string.IsNullOrEmpty(output))
+    {
+        output += ' ';
+    }
+
+    while (idx < input.Length && input[idx] != ' ')
+    {
+        output += input[idx];
+        idx++;
+    }
+
+    while (idx < input.Length && input[idx] == ' ')
+    {
+        idx++;
+    }
+}
+
+Console.WriteLine($"Compressed sentence: \"{output}\"");
