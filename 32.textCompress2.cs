fdefab9f31f86f34940dee65870b68066ee8748a
Nevena <nevena.rogova@gmx.at>
2025-02-25 16:21:41 +0200

    Add files via upload

---
+++ 32.textCompress2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter a sentence: ");
+        string input = Console.ReadLine();
+
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Compressed sentence: \"\"");
+            return;
+        }
+
+        StringBuilder output = new StringBuilder();
+        bool insideWord = false;
+
+        foreach (char c in input)
+        {
+            if (!char.IsWhiteSpace(c))
+            {
+                output.Append(c);
+                insideWord = true;
+            }
+            else if (insideWord)
+            {
+                output.Append(' ');
+                insideWord = false;
+            }
+        }
+        string result = output.ToString().TrimEnd();
+        Console.WriteLine($"Compressed sentence: \"{result}\"");
+    }
+}
