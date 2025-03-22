127e6a96157c985b9721e004bd7559a908c9e293
Nevena <nevena.rogova@gmx.at>
2025-03-22 21:32:21 +0200

    Add files via upload

---
+++ 35.DemoMethod .cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+int ReadNumber()
+{
+    Console.Write("Bitte Zahl eingeben: ");
+
+    string input = Console.ReadLine();
+
+    int number;
+
+    while (!int.TryParse(input, out number))
+    {
+        Console.WriteLine("Fehler, bitte wiederholen");
+        Console.Write("Bitte Zahl eingeben: ");
+        input = Console.ReadLine();
+    }
+
+    return number;
+}
+
+int ReadNumberWithMessage(string message, int min, int max)
+{
+    string msg = $"{message} [{min}..{max}] ";
+    Console.Write(msg);
+
+    string input = Console.ReadLine();
+
+    int number;
+
+    while (!int.TryParse(input, out number) ||
+           number < min ||
+           number > max)
+    {
+        Console.WriteLine("Fehler, bitte wiederholen");
+        Console.Write(msg);
+        input = Console.ReadLine();
+    }
+
+    return number;
+}
+
+
+int number1 = ReadNumberWithMessage("Bitte geben sie Minimum-Wert ein", 1, 50);
+int number2 = ReadNumberWithMessage("Bitte geben sie Maximum-Wert ein", 50, 100);
+int number3 = ReadNumber();
+
+Console.WriteLine($"{number1}-{number2}-{number3}");
