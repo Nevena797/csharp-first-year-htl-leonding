c2ebd029631e5c1e6a3813d5ab32b5a094568ee6
Nevena <nevena.rogova@gmx.at>
2025-03-22 20:10:00 +0200

    Update 05.Greetings1.cs

--- 05.Greetings1.cs
+++ 05.Greetings1.cs
@@ -1,10 +1,22 @@
-﻿namespace _05.Greetings1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+string firstName;
+string lastName;
+string city;
+
+Console.Write("Bitte geben Sie Ihren Vornamen ein: ");
+firstName = Console.ReadLine();
+
+Console.Write("Bitte geben Sie Ihren Nachnamen ein: ");
+lastName = Console.ReadLine();
+
+Console.Write("Bitte geben Sie Ihren Wohnort ein: ");
+city = Console.ReadLine();
+
+
+Console.Write("Herzlich Willkommen ");
+Console.Write(firstName);
+Console.Write(" ");
+Console.Write(lastName);
+Console.Write(" aus ");
+Console.Write(city);
+Console.WriteLine(" zu C#!");
+
