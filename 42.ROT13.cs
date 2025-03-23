a855fae4560c298a9b277df1f6621a9329a422bf
Nevena <nevena.rogova@gmx.at>
2025-03-23 13:53:32 +0200

    Add files via upload

---
+++ 42.ROT13.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter a text: ");
+        string text = Console.ReadLine();
+
+        string encrypted = Rot13(text);
+
+        Console.WriteLine($"Plain: {text}");
+        Console.WriteLine($"Encrypted: {encrypted}");
+        Console.WriteLine($"Decrypted: {Rot13(encrypted)}");
+    }
+
+    static string Rot13(string text)
+    {
+        char[] result = text.ToCharArray();
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (result[i] >= 'a' && result[i] <= 'z')
+            {
+                int newPos = ((result[i] - 'a' + 13) % 26) + 'a';
+                result[i] = (char)newPos;
+            }
+        }
+
+        return new string(result);
+    }
+}
