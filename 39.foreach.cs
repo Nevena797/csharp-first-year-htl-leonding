c201fe067d7acd9821047ad089257ac63cc18333
Nevena <nevena.rogova@gmx.at>
2025-03-23 10:45:32 +0200

    Add files via upload

---
+++ 39.foreach.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Hello";
+
+        foreach (char c in input)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
+
+//for (int i = 0; i < input.Length; i++)
+//Schreibgeschützt: Sie können Elemente nicht direkt in foreach ändern.
+//Indizierungsfehler vermeiden. Kein Index!
+//Funktioniert mit jeder Sammlung, die IEnumerable unterstützt – Arrays, Listen, Zeichenfolgen.
