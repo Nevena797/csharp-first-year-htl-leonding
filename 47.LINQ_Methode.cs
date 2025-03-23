a236821cbdbb72d38608c4bc23f952bae7c2f0bd
Nevena <nevena.rogova@gmx.at>
2025-03-23 15:19:59 +0200

    Update 47.LINQ_Methode.cs

--- 47.LINQ_Methode.cs
+++ 47.LINQ_Methode.cs
@@ -1,6 +1,26 @@
-﻿d => !userDigits.Contains(d);
+using System;
+using System.Collections.Generic;
+using System.Linq; // Notwendig für die Verwendung von `Where`
 
-string allDigits = "123456789";
-HashSet<char> userDigits = new() { '1', '3', '5' };
+class Program
+{
+    static void Main()
+    {
+        // Definiere eine Zeichenkette mit allen Ziffern von 1 bis 9
+        string allDigits = "123456789";
 
-var missingDigits = allDigits.Where(d => !userDigits.Contains(d));
+        // Erstelle ein HashSet mit einigen bereits vorhandenen Ziffern
+        HashSet<char> userDigits = new() { '1', '3', '5' };
+
+        // Verwende LINQ, um die fehlenden Ziffern zu finden
+        var missingDigits = allDigits.Where(d => !userDigits.Contains(d));
+
+        // Gib die fehlenden Ziffern aus
+        Console.WriteLine("Folgende Ziffern fehlen:");
+        foreach (var digit in missingDigits)
+        {
+            Console.Write(digit + " ");
+        }
+    }
+}
+
