30572e792f4c1ba6575e288a880f30bed722e3e0
Nevena <nevena.rogova@gmx.at>
2025-03-23 14:15:03 +0200

    Add files via upload

---
+++ 44.WordCount1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+Console.WriteLine("Count words in a sentence");
+Console.WriteLine("****************************");
+
+while (true)
+{
+    Console.Write("Sentence: ");
+    string input = Console.ReadLine();
+
+
+    if (IsEnd(input))
+    {
+        Console.WriteLine("Program terminated.");
+        break;
+    }
+
+
+    int wordCount = CountWords(input);
+
+
+    Console.WriteLine($"The sentence \"{input}\" has {wordCount} words.");
+}
+
+
+
+static bool IsEnd(string input)
+{
+    return input.Trim().ToLower() == "end";
+}
+
+
+static int CountWords(string sentence)
+{
+    if (string.IsNullOrWhiteSpace(sentence))
+    {
+        return 0;
+    }
+
+    string[] words = sentence.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    return words.Length; 
+}
+
