e64fb9d6de64534397ee37874f5c75bddd3e0465
Nevena <nevena.rogova@gmx.at>
2025-03-23 10:42:36 +0200

    Add files via upload

---
+++ 38.HashSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<char> userDigits = new();
+
+        userDigits.Add('1');
+        userDigits.Add('2');
+        userDigits.Add('2'); // Wird ignoriert, da „2“ bereits vorhanden ist
+
+        foreach (char c in userDigits)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
