8a84ed1f6fe2952ff26e4e147b595d8d0727b74e
Nevena <nevena.rogova@gmx.at>
2025-03-23 11:50:21 +0200

    Add files via upload

---
+++ 01.HelloWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BSP1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Word!");
+            Console.WriteLine("===========");
+            Console.WriteLine();
+
+            Console.WriteLine("Britte druecken Sie die ENTER-Taste");
+            Console.ReadLine();
+        }
+    }
+}
