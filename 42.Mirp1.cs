6e83b9926a480bc66e6a9d403a160218e8831140
Nevena <nevena.rogova@gmx.at>
2025-03-23 14:44:22 +0200

    Add files via upload

---
+++ 42.Mirp1.cs
@@ -0,0 +1,47 @@
+﻿int number = 0;
+int reversed = reversedNumber(number);
+
+for (int i = 0; i < 1000; i++)
+{
+    if (isPrim(i) && isPrim(reversedNumber(i)))
+    {
+        Console.Write(i);
+        if (number == 10)
+        {
+            Console.WriteLine();
+            number = 0;
+        }
+        else
+        {
+            Console.Write(", ");
+            number++ ;
+        }
+    }
+
+}
+int reversedNumber(int inputNumber)
+{
+    string firstNumber = inputNumber.ToString();
+    string outputNumber = "";
+    for (int i = firstNumber.Length - 1; i >= 0; i--)
+    {
+        outputNumber += firstNumber[i];
+    }
+    return Convert.ToInt32(outputNumber);
+}
+bool isPrim(int numberTwo)
+{
+    if (numberTwo == 1 || numberTwo == 2)
+    {
+        return true;
+    }
+    for (int i = 3; i < numberTwo; i += 2)
+    {
+        if (numberTwo % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+
+}
