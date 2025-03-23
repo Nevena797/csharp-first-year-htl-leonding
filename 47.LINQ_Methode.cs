26803d1c1c11b5d1ed3b1a1dcd18ad219c6a1988
Nevena <nevena.rogova@gmx.at>
2025-03-23 15:19:28 +0200

    Add files via upload

---
+++ 47.LINQ_Methode.cs
@@ -0,0 +1,6 @@
+﻿d => !userDigits.Contains(d);
+
+string allDigits = "123456789";
+HashSet<char> userDigits = new() { '1', '3', '5' };
+
+var missingDigits = allDigits.Where(d => !userDigits.Contains(d));
