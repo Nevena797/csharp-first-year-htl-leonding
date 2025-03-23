1321936445c31fcce436d193707ed62d30c9e1f3
Nevena <nevena.rogova@gmx.at>
2025-03-23 11:23:49 +0200

    Add files via upload

---
+++ 10grounUpCalculator1.cs
@@ -0,0 +1,48 @@
+﻿onsole.Write("Please enter your age: ");
+int alter = int.Parse(Console.ReadLine());
+
+if (alter >= 18 && alter <= 20)
+{
+    Console.WriteLine("Hello, you're grown up!");
+}
+else if (alter < 18)
+{
+    Console.WriteLine("Years must be 18!");
+}
+else if (alter >= 65 && alter <= 120)
+{
+    Console.WriteLine("Hello Senior!");
+}
+else if (alter > 120)
+{
+    Console.WriteLine("Zombie");
+}
+else
+{
+    Console.WriteLine("Nice to see you");
+}
+
+
+Console.Write("Please enter your age again: ");
+alter = int.Parse(Console.ReadLine());
+
+if (alter == 25 || alter == 50 || alter == 75)
+{
+    Console.WriteLine("Happy milestone birthday!");
+}
+else if (alter < 15)
+{
+    Console.WriteLine("It will still take a long time until you turn 18!");
+}
+else if (alter >= 65 && alter <= 120)
+{
+    Console.WriteLine("Hello Senior!");
+}
+else if (alter > 120)
+{
+    Console.WriteLine("Zombie");
+}
+else
+{
+    Console.WriteLine("Nice to see you");
+}
