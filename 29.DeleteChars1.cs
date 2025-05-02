be0de184adbecd9c14732ca97042879eb410bbfe
Nevena <nevena.rogova@gmx.at>
2025-05-02 13:54:31 +0300

    Add files via upload

---
+++ 29.DeleteChars1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _29.DeleteChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText;
+            string eliminatorText;
+            string result = "";
+
+            Console.WriteLine("DeleteChars");
+            Console.WriteLine("===========");
+            Console.WriteLine();
+            Console.Write("Eingabetext: ");
+            inputText = Console.ReadLine();
+            Console.Write("Eliminatortext: ");
+            eliminatorText = Console.ReadLine();
+
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                bool toBeDeleted = false;
+
+                for (int j = 0; j < eliminatorText.Length; j++)
+                {
+                    if (inputText[i] == eliminatorText[j])
+                    {
+                        toBeDeleted = true;
+                        break;
+                    }
+                }
+
+                if (!toBeDeleted)
+                {
+                    result += inputText[i];
+                }
+            }
+
+            Console.WriteLine("Ergebnis: " + result);
+            Console.WriteLine("Drücken Sie eine beliebige Taste...");
+            Console.ReadKey();
+        }
+    }
+}
