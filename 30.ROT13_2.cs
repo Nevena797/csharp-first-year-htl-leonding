39b8534ec8016da0cfb3e6994d3e1ab7a2e65627
Nevena <nevena.rogova@gmx.at>
2025-05-13 10:45:30 +0300

    Add files via upload

---
+++ 30.ROT13_2.cs
@@ -0,0 +1,10 @@
+﻿namespace _30.ROT13_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
